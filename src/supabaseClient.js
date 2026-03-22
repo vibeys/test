7e94c622a8b74228a5cd835a0814336bf59d659a
vibeys <luismengote9@gmail.com>
2026-03-22 16:17:57 +0800

    hello

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://bmwlwbydwlrpoyfawltm.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtd2x3Ynlkd2xycG95ZmF3bHRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5ODEzMzksImV4cCI6MjA4OTU1NzMzOX0.qWAu_lpKG5Fk8YU2tblaP88g4OKOhZMO71APf7dE_ao"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
